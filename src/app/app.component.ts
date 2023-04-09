b16d0af1d90d705135420fb5306960024b298728
shiwani <shiwanipaunikar1@gmail.com>
2023-04-09 22:45:42 +0530

    Added:check validation userName is valid or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl="https://cdn.telanganatoday.com/wp-content/uploads/2021/09/Bridgelabzz.jpg";
   url="https://bridgelabz.com/";
   userName:string = "";
+  nameError:string = "";
 
   ngOnInit():void{
     this.title = "Hello From Bridgelabz."
@@ -18,5 +19,14 @@ export class AppComponent {
   onClick($event:any){
     console.log("Save button is clicked!",$event);
     window.open(this.url,"_blanck")
+  }
+
+  onInput($event:any){
+    console.log("change event occured!",$event.data);
+    const nameRegex=RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }this.nameError = "Name is Incorrect";
   }
 }
