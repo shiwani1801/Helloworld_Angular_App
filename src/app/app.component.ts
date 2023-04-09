de74cd863d160eeb0c15d8b5a76510522b2f2c34
shiwani <shiwanipaunikar1@gmail.com>
2023-04-09 13:42:52 +0530

    Added:click on logo using event binding and show the website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl="https://cdn.telanganatoday.com/wp-content/uploads/2021/09/Bridgelabzz.jpg";
+  url="https://bridgelabz.com/";
 
   ngOnInit():void{
     this.title = "Hello From Bridgelabz."
+  }
+  
+  onClick($event:any){
+    console.log("Save button is clicked!",$event);
+    window.open(this.url,"_blanck")
   }
 }
